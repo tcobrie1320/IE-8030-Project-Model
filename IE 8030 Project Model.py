ac2e76804bc5960527d8d6639b9ab71a17975521
Tyler O'Brien <tcobrie@g.clemson.edu>
2021-04-09 15:53:54 -0400

    Update IE 8030 Project Model.py

--- IE 8030 Project Model.py
+++ IE 8030 Project Model.py
@@ -1611,5 +1611,8 @@ model.y = Var(model.F,model.C, within=NonNegativeReals)
 
 # Objective Function
 def ObjectiveFunction(model):
-    return (1/Count_F)*sum
+    return (1/Count_F)*(sum(model.r[s,f]*model.x[s,f])+(1/Count_C)
+                        *(sum(model.k[f,c]*model.y[f,c]) for s in model.S 
+                        for f in model.F for c in model.C)
+model.obj = Objective(rule = ObjectiveFunction, sense = minimize)
 # Amount Sent from API Site (in set S) to Manufacturing Site (in set F)
