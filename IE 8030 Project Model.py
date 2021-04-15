de79742c9c6d1f2db668707a8b30482420552fe0
Tyler O'Brien <tcobrie@g.clemson.edu>
2021-04-15 16:08:08 -0400

    Fixed Objective Function

--- IE 8030 Project Model.py
+++ IE 8030 Project Model.py
@@ -1618,7 +1618,7 @@ model.y = Var(model.F,model.C, within=NonNegativeReals)
 #Objective Function
 def ObjectiveFunction(model):
     return (1/Count_F)*(sum(model.r[s,f]*model.x[s,f]for s in model.S 
-                        for f in model.F)+(1/Count_C)  )*(sum(model.k[f,c]*model.y[f,c] 
+                        for f in model.F)+(1/Count_C)  )+(sum(model.k[f,c]*model.y[f,c] 
                         for s in model.S  for f in model.F for c in model.C))
 model.obj = Objective(rule=ObjectiveFunction, sense = minimize)
 
